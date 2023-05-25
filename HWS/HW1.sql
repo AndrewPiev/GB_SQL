dc81d6fe9ef668ff2a25cf5812f42db34bca14ac
Andrew Piev <7841588@gmail.com>
2023-05-25 22:19:36 +0300

    HW1

---
+++ HWS/HW1.sql
@@ -0,0 +1,19 @@
+SELECT ProductName, Manufacturer, Price
+FROM phones.new_table
+WHERE ProductCount > 2;
+
+SELECT *
+FROM phones.new_table
+WHERE Manufacturer = "Samsung";
+
+SELECT *
+FROM phones.new_table
+WHERE ProductName LIKE '%iPhone%';
+
+SELECT *
+FROM phones.new_table
+WHERE Manufacturer LIKE '%Samsung%';
+
+SELECT *
+FROM phones.new_table
+WHERE ProductName LIKE '%8%';
